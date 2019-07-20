fb5661b99c087fc881853c5371ba0cbb1e426f41
grishmagurbani <grishmagurbani1998@gmail.com>
2019-07-20 22:05:03 +0530

    catalog done

--- Trishul/src/app/app-routing.module.ts
+++ Trishul/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "landing", component: LandingComponent},
+  {path: "catalog", component: CatalogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
