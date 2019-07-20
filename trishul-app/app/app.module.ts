31d9bfb861e356e8c7bea872599aa4114286a3d9
grishmagurbani <grishmagurbani1998@gmail.com>
2019-07-20 19:24:40 +0530

    test

--- trishul-app/app/app.module.ts
+++ trishul-app/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
@@ -15,6 +17,8 @@ import { HomeComponent } from './home/home.component';
   declarations: [
       AppComponent,
       HomeComponent,
+      LandingPageComponent,
+      CatalogComponent,
   ],
   imports: [
       NativeScriptModule,
--- trishul-app/app/home/home.component.ts
+++ trishul-app/app/home/home.component.ts
@@ -11,14 +11,4 @@ export class HomeComponent {
   private counter = 42;
 
   constructor() { }
-
-  public getMessage() {
-    return this.counter > 0 ?
-      `${this.counter} taps left` :
-      'Hoorraaay! You unlocked the NativeScript clicker achievement!';
-  }
-
-  public onTap() {
-    this.counter--;
-  }
 }
