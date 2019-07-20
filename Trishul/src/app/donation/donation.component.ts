2c476e6574cac8cea6341f1b200be3b9f22ed5d9
Mihir Deodhar <mihirnd@gmail.com>
2019-07-20 20:51:14 +0530

    Donations partially done

--- Trishul/src/app/donation/donation.component.ts
+++ Trishul/src/app/donation/donation.component.ts
@@ -9,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DonationComponent implements OnInit {
   donationForm: FormGroup;
+  cause = [];
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.donationForm = fb.group({
       cause: this.fb.control(''),
@@ -19,13 +20,20 @@ export class DonationComponent implements OnInit {
   ngOnInit() {
     this.http.get('http://10.49.148.116:8000/api/getCause').subscribe(
       res => {
-        console.log(res);
+        this.cause = res["data"];
+        console.log(this.cause);
       }
     );
   }
+  log(i: number) {
+    console.log(i);
+  }
 
   donate() {
-    this.http.post('http://10.49.148.116:8000/api/submitDonation', { cause_id: 1, amount: 10000, customer_id: 1 }).subscribe(
+    console.log(this.donationForm.value);
+    
+    const obj = {customer_id: 1, cause_id: 1, amount: 10000 };
+    this.http.post('http://10.49.148.116:8000/api/submitDonation', obj).subscribe(
 
       res => {
         console.log(res);
