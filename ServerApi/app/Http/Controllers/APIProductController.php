d3af75d40bc723d43886d515bdb15940a8e0ee60
UjalaJha <jhaujala3@gmail.com>
2019-07-20 18:43:07 +0530

    Added Files

--- ServerApi/app/Http/Controllers/APIProductController.php
+++ ServerApi/app/Http/Controllers/APIProductController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Product;
+use App\ProductCategory;
+use App\Http\Resources\GeneralResources as GeneralResources;
 class APIProductController extends Controller
 {
-    //
+    function getcategory(){
+    	$category=ProductCategory::with('products')->get();
+    	// print_r($category);
+    	// exit;
+    	$dataModel=[];
+    	if($category){
+        	$dataModel['data'] = $category;
+        	$dataModel['message'] = "Fetch Successful";
+        	$dataModel['error'] = false;
+        }else{
+        	$dataModel['data'] = null;
+        	$dataModel['message'] = "Fetch Unsuccessful";
+        	$dataModel['error'] = true;
+        }
+        return new GeneralResources($dataModel);
+    }
 }
--- ServerApi/app/Product.php
+++ ServerApi/app/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'product';
+    protected $primaryKey = 'product_id';
+    public $timestamps = false;
+    public function category(){
+        return $this->belongsTo('App\ProductCategory','category_id');
+    }
 }
