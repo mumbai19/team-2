4cb0759efbf8b7c9ede37e35ad020a91c4dca1cf
UjalaJha <jhaujala3@gmail.com>
2019-07-21 09:36:14 +0530

    Screen for Apps

--- sidemenu/src/app/pages/donations/donations.page.ts
+++ sidemenu/src/app/pages/donations/donations.page.ts
@@ -43,7 +43,7 @@ export class DonationsPage implements OnInit {
     details.amount = 100;
     // userDetails.username = data.username;
 
-    this.http.post('http://localhost:8000/api/submitDonation', details).subscribe(
+    this.http.get('http://localhost:8000/api/payment', details).subscribe(
 
 
       //const obj = {customer_id: 1, cause_id: this.causeId, amount: this.donationForm.value.amount };
